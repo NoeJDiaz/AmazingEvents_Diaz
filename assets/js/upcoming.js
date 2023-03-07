8217f99782c1c8c3f9d9cec81996eafdf00e912d
NoeJDiaz <diaz.noelia.j@gmail.com>
2023-03-07 12:11:41 -0300

    js-dinamic

---
+++ assets/js/upcoming.js
@@ -0,0 +1,60 @@
+import data from "./amazing.js"
+
+function fillCard(image, name, date, description) {
+  let card = (`<div class="card1">
+              <img class="foto" src="${image}" width="300" height="300" alt=" concierto">
+  
+              <h2 class="titulo">${name}</h2>
+               <p class="texto">
+                  ${description}
+              </p>
+              <div class="acceso">
+                  <p class="codigo">${date}</p>
+                   <button class="btn btn1 btn-outline-danger " type="submit">Search</button>
+              </div>
+              </div>`)
+
+  return card;
+
+}
+// recibe todos los eventos y la fecha comparada
+function dateEvents(events, date) {
+  //  array me va a guardar las card filtradas por la fecha y completas con los datos
+  let filterEvents = [];
+  // recorre cada uno de los eventos(array)    
+  for (const event of events) {
+
+    //compara  la fecha de cada uno de los eventos
+    if (new Date(event.date).getTime() > new Date(date).getTime()) {
+      //separa los eventos filtrados
+      let newCard = fillCard(event.image, event.name, event.date, event.description)
+      // guarda los eventos en el array
+      filterEvents.push(newCard)
+    }
+
+  }
+  //retorta el array nuevo
+  return filterEvents;
+}
+
+
+
+
+//recibe toda la data
+function rendercard(dataEvents) {
+  //selecciona el div que contiene las cards
+  const cardss = document.getElementById('cardss')
+  // me guarda al array // me devuelve el array de eventos filtrados con la informacion completa
+  let cards = dateEvents(dataEvents.events, dataEvents.currentDate);
+  // recorre cada card
+  for (const card of cards) {
+
+    cardss.innerHTML += card;
+
+  }
+
+}
+
+
+
+rendercard(data);
